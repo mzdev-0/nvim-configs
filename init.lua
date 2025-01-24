cabdd0fa20785b2e53829573c45698ff7a0473f4
Casp3r-0 <michzadc@gmail.com>
2025-01-24 15:49:48 -0600

    add: auto-update lazy on nvim start

--- init.lua
+++ init.lua
@@ -4,3 +4,12 @@ require("config.lazy")
 vim.wo.wrap = true
 vim.wo.relativenumber = false
 vim.wo.number = true
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    desc = "Auto-update plugins",
+    callback = function()
+        -- Run `:Lazy sync` in the background (non-blocking)
+        require("lazy").sync({ wait = false })
+    end,
+    once = true, -- Ensure the autocommand only runs once per session
+})
