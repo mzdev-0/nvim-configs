a7c671dd860075d455dcc3373aa811c6be3e6d83
mzdev-0 <github@ghostfrnd.co>
2025-05-30 15:04:33 -0500

    update init

--- init.lua
+++ init.lua
@@ -1,27 +1,9 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Leader Key Map
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
--- Load core config
-require("config.autocmds")
-require("config.keymaps")
-require("config.options")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
 -- Disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
 
 -- General settings
 vim.wo.wrap = true
@@ -31,51 +13,3 @@ vim.opt.termguicolors = true
 vim.opt.timeoutlen = 300
 vim.g.project_root = vim.fn.getcwd(-1)
 vim.o.clipboard = "unnamedplus"
-
--- Initialize lazy.nvim plugins
-require("lazy").setup("plugins", { -- "plugins" refers to lua/plugins/init.lua
-    install = { colorscheme = { "tokyonight", "habamax" } },
-    checker = { enabled = true },
-})
-
--- Auto update on startup
-vim.api.nvim_create_autocmd("VimEnter", {
-    desc = "Auto-update plugins",
-    callback = function()
-        require("lazy").sync({ wait = false, show = false })
-    end,
-    once = true,
-})
-
--- Enable LSPs
-vim.lsp.enable({
-    "ansible",
-    "bash",
-    "cpp",
-    "docker-compose",
-    "docker",
-    "gh_actions",
-    "go",
-    "helm",
-    "html",
-    "htmx",
-    "hypr",
-    "lua",
-    "nginx",
-    "powershell",
-    "python",
-    "terraform",
-    "typescript",
-    "yaml",
-})
-
--- Integrate OSC52 for remote copying
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-        local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
-        copy_to_unnamedplus(vim.v.event.regcontents)
-        local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
-        copy_to_unnamed(vim.v.event.regcontents)
-    end,
-})
